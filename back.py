ea7fb76813c7cd72aef9e4b1d2a09458641e557c
ArturGrigoryan1 <143454080+ArturGrigoryan1@users.noreply.github.com>
2024-03-20 19:20:46 +0400

    Update back.py

--- back.py
+++ back.py
@@ -2,7 +2,7 @@ import os
 var = os.getenv("hash")
 with open('docker-compose.yml', 'r+') as f:
     lines = f.readlines()
-    index = lines[11].find(":", 15]
+    index = lines[11].find(":", 15)
     lines[11] = (lines[11])[:index+1] + str(var) + "\n"
     f.seek(0)
     for line in lines:
